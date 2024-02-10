ac94318d9a6bcea032c6dff0c26ba8eda05ecb75
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-02-10 15:29:27 -0500

    Create SideMenuView file

---
+++ SideMenuDemo/SideMenuView.swift
@@ -0,0 +1,22 @@
+//
+//  SideMenuView.swift
+//  SideMenuDemo
+//
+//  Created by Leonard McCook-Carr on 2/10/24.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    @Binding var isShowing: Bool
+    
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+#Preview {
+    SideMenuView(isShowing: .constant(false))
+}
