b0174f4ab1865a56cd3bf6cb92d85fb648a71786
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-02-14 09:04:31 -0500

    Create SideMenuRowView file

---
+++ SideMenuDemo/SideMenuRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuRowView.swift
+//  SideMenuDemo
+//
+//  Created by Leonard McCook-Carr on 2/14/24.
+//
+
+import SwiftUI
+
+struct SideMenuRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuRowView()
+}
